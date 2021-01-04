4fc6919dbb39504605fe51615a00c19c1d8161ec
Heliel Amaro <hfamaro@hotmail.com>
2021-01-04 11:39:24 -0400

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,18 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Department department = new Department(1,"Setec");
+		Seller seller = new Seller(1,"Heliel","hfamaro@hotmailcom", new Date(),5000.00,department);
+		
 		System.out.println(department);
+		System.out.println(seller);
 
 	}
 
