31bf4a5212a2042ea4c13671f6ce102efca84526
Heliel Amaro <hfamaro@hotmail.com>
2021-01-04 11:58:20 -0400

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -10,6 +12,8 @@ public class Program {
 	public static void main(String[] args) {
 		Department department = new Department(1,"Setec");
 		Seller seller = new Seller(1,"Heliel","hfamaro@hotmailcom", new Date(),5000.00,department);
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
 		
 		System.out.println(department);
 		System.out.println(seller);
